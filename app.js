a56cfa7a140766b5b5398fba12f38b529821017e
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2022-12-18 08:16:27 +0100

    Add header

---
+++ app.js
@@ -0,0 +1,64 @@
+const CALC_DISPLAY = document.querySelector('.calc-display');
+const BUTTONS = document.querySelectorAll('button');
+
+let result = 0;
+let finalResult = 0;
+let userInput = '';
+
+// Fonction d'opération
+const operate = function(op, a, b) {
+  switch (op) {
+    case '+':
+      add(a, b);
+      return result;
+      break;
+    case '-':
+      subtract(a, b);
+      return result;
+      break;
+    case '*':
+      multiply(a, b);
+      return result;
+      break;
+    case '/':
+      divide(a, b);
+      return result;
+      break;
+  }
+}
+
+// Fonctions de calcul
+const add = function(a, b) {
+  result = a + b;
+}
+
+const subtract = function(a, b) {
+  result = a - b;
+}
+
+const multiply = function(a, b) {
+  return result = a * b;
+}
+
+const divide = function(a, b) {
+  return result = a / b;
+}
+
+// Fonctions d'affichage des entrées et du résultat
+const updateDisplay = function() {
+  CALC_DISPLAY.textContent = userInput;
+}
+
+for (let i = 0; i < BUTTONS.length; i++) {
+  BUTTONS[i].addEventListener('click', function() {
+    userInput += this.textContent;
+    updateDisplay();
+    }
+  )
+}
+
+/*
+Si l'input est un chiffre, et que input1 = undefined, enregistrer l'input dans input1. Autrement, enregistrer l'input comme input2.
+Si l'input est un operateur, enregistrer dans op. a chaque fois qu'on appuye sur un bouton operateur ou sur 'égal', 
+exécuter operate().
+*/
