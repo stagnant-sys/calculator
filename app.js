3f97d3f37a66d901f3d2e647b227e0808ea8bdb3
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2022-12-18 09:10:50 +0100

    Working a and op

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const CALC_DISPLAY = document.querySelector('.calc-display');
-const BUTTONS = document.querySelectorAll('button');
+const NUMBERS_BUTTONS = document.querySelectorAll('.number-btn');
+const OPERATORS_BUTTONS = document.querySelectorAll('.operator-btn');
 
-let result = 0;
+let tempResult = 0;
 let finalResult = 0;
 let userInput = '';
+let op;
+let a;
+let b;
 
 
 // Fonction d'opération
@@ -11,26 +15,26 @@ const operate = function(op, a, b) {
   switch (op) {
     case '+':
       add(a, b);
-      return result;
+      return tempResult;
       break;
     case '-':
       subtract(a, b);
-      return result;
+      return tempResult;
       break;
     case '*':
       multiply(a, b);
-      return result;
+      return tempResult;
       break;
     case '/':
       divide(a, b);
-      return result;
+      return tempResult;
       break;
   }
 }
 
 // Fonctions de calcul
 const add = function(a, b) {
-  result = a + b;
+  tempResult = a + b;
 }
 
 const subtract = function(a, b) {
@@ -38,26 +42,44 @@ const subtract = function(a, b) {
 }
 
 const multiply = function(a, b) {
-  return result = a * b;
+  tempResult = a * b;
 }
 
 const divide = function(a, b) {
-  return result = a / b;
+  tempResult = a / b;
 }
 
 // Fonctions d'input et de sélectiond'affichage des entrées et du résultat
-for (let i = 0; i < BUTTONS.length; i++) {
-  BUTTONS[i].addEventListener('click', function() {
+const getNumberInputs = function() {
+  userInput += (this.textContent);
+  updateDisplay();
+}
+
+const getOperator = function() {
+  if (!a) {
+    a = parseInt(userInput);
+    op = this.textContent;
     userInput += (this.textContent);
     updateDisplay();
-    console.log(parseInt(userInput));
-    }
-  )
+  } else {
+    CALC_DISPLAY.textContent = ' ';
+    b = parseInt(userInput);
+  } 
 }
 
 const updateDisplay = function() {
   CALC_DISPLAY.textContent = userInput;
 }
+
+for (let i = 0; i < NUMBERS_BUTTONS.length; i++) {
+  NUMBERS_BUTTONS[i].addEventListener('click', getNumberInputs)
+}
+
+for (let i = 0; i < OPERATORS_BUTTONS.length; i++) {
+  OPERATORS_BUTTONS[i].addEventListener('click', getOperator)
+}
+
+
 
 //Fonctions de calcul
 
